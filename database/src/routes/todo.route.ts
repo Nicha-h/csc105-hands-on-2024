bccd9e4736e197f47e26acdfcfad1ca162bee585
Nicha-h <135990448+Nicha-h@users.noreply.github.com>
2025-04-22 16:20:12 +0700

    Intro to Database

---
+++ database/src/routes/todo.route.ts
@@ -0,0 +1,11 @@
+import { Hono } from "hono";
+import * as todoController from "../controllers/todo.controller.ts";
+
+const todoRouter = new Hono();
+todoRouter.post("/", todoController.createTodo);
+todoRouter.get("/", todoController.getTodo);
+todoRouter.patch("/:id/complete", todoController.completeTodo);
+todoRouter.patch("/:id/title", todoController.updateTodoTitle);
+todoRouter.get("/user/:id", todoController.getTodosOfUser);
+todoRouter.delete("/", todoController.deleteTodo);
+export { todoRouter };
